41d369f56b51b4c8173f6d62e503a428de318fe5
Simon Plenderleith <simon@simonplend.co.uk>
2022-04-26 21:07:51 +0100

    Restructure the project

---
+++ bin/cli.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const { search } = require("../lib/search.js");
+
+search(process.argv[2]);
--- scripts/pull-changelogs.mjs
+++ scripts/pull-changelogs.mjs
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 import * as fs from "node:fs";
 import * as path from "node:path";
